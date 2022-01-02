1fbc1426bb8429beda2394e5d023b700cafe3268
Dario Prazeres <darioedgarprazeres@gmail.com>
2022-01-03 00:20:39 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const table_1 = document.querySelector('div.table-1');
 var number;
 const array = [];
 var position = [];
-var width = 10
+var width = 10;
+var positionSelected = [];
 function gameBoard(){
     table_1.style.gridTemplateColumns = `repeat(${width}, 1fr)`
     table_1.style.gridTemplateRows = `repeat(${width}, 1fr)`  
@@ -11,14 +12,21 @@ function gameBoard(){
         const row = document.createElement('div');
         array.push(row);
         row.classList.add('row');
-        row.addEventListener('click', ()=>{            
-            console.log(position[cont]);
-            row.classList.add('row-1');
+        row.addEventListener('click', ()=>{     
+            var pos = position[cont];      
+            console.log(pos);
+            if(pos % 9 == 1){
+                alert('INCORECT PLACE')
+            }else{
+                for(let i = pos; i < pos + 4; i++){
+                    array[i].classList.add('row-1');
+                    console.log(i);
+                }
+            }
+            
+            
         });        
-        row.addEventListener('dblick', ()=>{
-            console.log(array[cont]);
-            row.classList.add('row');
-        });   
+         
         table_1.appendChild(row);
     }
 }
