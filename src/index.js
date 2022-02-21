5c554543d3778d167a7baaab4ddcc8d4348fc6e6
Dario Prazeres <darioedgarprazeres@gmail.com>
2022-02-22 00:11:06 +0100

    update index

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import gameBoard from './gameBoardGUI';
 //import GameBoard from './gameBoard';
 import gameBoardPC from './playerPc';
 import showNumber from './ramdomXY';
+gameBoard();
+gameBoardPC();
